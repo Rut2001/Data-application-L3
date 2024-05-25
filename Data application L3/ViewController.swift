63bc11af72aebf654ef1c4e80ca38991cd0b7f31
Rut patel <rutpatel359@gmail.com>
2024-05-25 11:17:00 -0700

    Initial Commit

---
+++ Data application L3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Data application L3
+//
+//  Created by user240207 on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Data application L3UITests/Data_application_L3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Data_application_L3UITestsLaunchTests.swift
+//  Data application L3UITests
+//
+//  Created by user240207 on 5/25/24.
+//
+
+import XCTest
+
+final class Data_application_L3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
